06a55cae244319a9b2e6290a4015fb32c6c7d4d4
Ross Reicks <rreicks13@gmail.com>
2025-02-23 10:25:01 -0600

    chore: try to url encode the connection string

--- src/config/database.config.ts
+++ src/config/database.config.ts
@@ -2,7 +2,9 @@ import type { TypeOrmModuleOptions } from '@nestjs/typeorm';
 
 export const databaseConfig: TypeOrmModuleOptions = {
   type: 'postgres',
-  url: process.env.DATABASE_URL,
+  url: process.env.DATABASE_URL
+    ? encodeURI(process.env.DATABASE_URL)
+    : undefined,
   autoLoadEntities: true,
   synchronize: false,
   ssl: false,
