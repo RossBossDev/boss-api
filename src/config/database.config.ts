4a65dc709891b9fc146e1333a72f33b9d9685917
Ross Reicks <rreicks13@gmail.com>
2025-02-23 10:29:43 -0600

    chore: remove connection string in favor of multiple variables

--- src/config/database.config.ts
+++ src/config/database.config.ts
@@ -2,9 +2,11 @@ import type { TypeOrmModuleOptions } from '@nestjs/typeorm';
 
 export const databaseConfig: TypeOrmModuleOptions = {
   type: 'postgres',
-  url: process.env.DATABASE_URL
-    ? encodeURI(process.env.DATABASE_URL)
-    : undefined,
+  host: process.env.DB_HOST || 'localhost',
+  port: Number.parseInt(process.env.DB_PORT || '5432'),
+  username: process.env.DB_USER || 'postgres',
+  password: process.env.DB_PASSWORD || 'postgres',
+  database: process.env.DB_NAME || 'boss_db',
   autoLoadEntities: true,
   synchronize: false,
   ssl: false,
